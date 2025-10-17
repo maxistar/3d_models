6e27ee3cf924abc241162c31ff4af1037c096592
Max Starikov <mstarikov@itemis.com>
2025-10-17 12:47:45 +0200

    add rotation

--- docs/src/components/STLViewerComponent.jsx
+++ docs/src/components/STLViewerComponent.jsx
@@ -12,6 +12,13 @@ export default function STLViewerComponent({ url }) {
         <div style={style}>
             <StlViewer
                 url={url}
+                orbitControls                // ← включает вращение/зум
+                shadows                      // тени на “пол”
+                showAxes                     // оси X/Y/Z
+                cameraProps={{               // подстрой стартовой камеры
+                    position: [2, 2, 2],       // подвинь при необходимости
+                    fov: 50
+                }}
                 modelColor="#B92E34"
                 backgroundColor="#EAEAEA"
                 rotate={true}
