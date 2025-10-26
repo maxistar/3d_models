d04d47400566771eb9e413f8a73c48eb6a06fef9
Max Starikov <mstarikov@itemis.com>
2025-10-26 08:30:34 +0100

    configure basepatch

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
-const repoName = '3d_models';
+const repoName = '/3d_models';
 
 // https://astro.build/config
 export default defineConfig({
-  site: `https://projects.maxistar.me/${repoName}/`,
+  site: `https://projects.maxistar.me${repoName}/`,
+  base: '/3d_models',
   integrations: [react()]
 });
