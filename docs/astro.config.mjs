4747282f1784110b9d43ddcc0fffbc4a3010f752
Max Starikov <mstarikov@itemis.com>
2025-10-26 08:25:06 +0100

    add base url

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+const repoName = '3d_models';
+
 // https://astro.build/config
 export default defineConfig({
+  site: `https://projects.maxistar.me/${repoName}/`,
   integrations: [react()]
 });
